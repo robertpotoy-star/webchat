bb25df5f49f26f5c259598fc51913bae5d6b0671
robertpotoy-star <robertpotoy@gmail.com>
2026-09-17 08:57:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -124,6 +124,15 @@ const server = http.createServer(async (req, res) => {
   }
 
   // ---- SERVE FRONTEND ----
+  if (req.url === '/__version') {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    version: 'v3-with-upgrade',
+    node: process.version,
+    hasUpgradeHandler: true
+  }));
+  return;
+}
   if (req.url === '/' || req.url === '/index.html') {
     const filePath = path.join(__dirname, 'public', 'index.html');
     fs.readFile(filePath, (err, data) => {
